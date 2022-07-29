ea3420fb3e782473bf8622c38b9b1f5b4e104bb0
daniel.lufupa <daniel.lufupa@dasa.com.br>
2022-07-28 22:44:43 -0300

    addes new logic ngfor

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -5,25 +5,23 @@ import { User } from 'src/app/services/User';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  // users: User[] = [];
+  use: any = [];
 
-  users: User[] = []
-  searchUser: any; 
-  constructor( private githubService : GithubService) {
-    this.getUser();
-   }
+  searchUser: string = '';
+  constructor(private githubService: GithubService) {}
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.getUser();
-    }, 300);
   }
 
   getUser(): void {
-  this.githubService.getGithubUser(this.searchUser).subscribe((user)=>(this.users = user));
-    console.log('O QUE VEMMMM', this.users);
-  }
+    this.use = [];
+    this.githubService.getGithubUser(this.searchUser).subscribe((data) => {
+      this.use.push(data) ;
+    });
 
+  }
 }
