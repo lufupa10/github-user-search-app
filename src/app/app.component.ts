0de65c401883ac0ce39ce7f225da3b6cd894c454
daniel.lufupa <daniel.lufupa@dasa.com.br>
2022-07-30 23:50:37 -0300

    added check internet and link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Observable, Observer, fromEvent, merge } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'github-user-search-app.git';
+  checkinterent: any;
+  checkConnection: string = '';
+  therichpost$() {
+    return merge<any>(
+      fromEvent(window, 'offline').pipe(map(() => false)),
+      fromEvent(window, 'online').pipe(map(() => true)),
+      new Observable((sub: Observer<boolean>) => {
+        sub.next(navigator.onLine);
+        sub.complete();
+      })
+    );
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.checkConnection =""
+      this.therichpost$().subscribe(
+        (isOnline) => (this.checkinterent = isOnline)
+      );
+
+      if (this.checkinterent == false) {
+        this.checkConnection = 'Sua internet não está funcionando :(';
+      }
+    }, 300);
+  }
 }
